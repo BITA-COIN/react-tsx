787a7337f5c91e7a289fe8f3b3947cd57f0a8d65
vs <project.coin2@gmail.com>
2021-12-24 00:47:54 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <p>
+          Edit <code>App</code> and.
+       </p>
+        <a
+          className="App-link"
+          href="/home"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
